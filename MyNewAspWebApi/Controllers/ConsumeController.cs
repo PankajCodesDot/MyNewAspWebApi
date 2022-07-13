8c596b70537b3799212715a8961264c4e3e984bd
pankaj <pankaj.codesdot@gmail.com>
2022-07-13 16:40:30 +0530

    Add project files.

---
+++ MyNewAspWebApi/Controllers/ConsumeController.cs
@@ -0,0 +1,134 @@
+﻿using MyNewAspWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyNewAspWebApi.Controllers
+{
+    public class ConsumeController : Controller
+    {
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<std> obj = new List<std>();
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+           var response= client.GetAsync("stulist");
+            response.Wait();
+            var test = response.Result;
+
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<std>>();
+                display.Wait();
+                obj = display.Result;
+            }
+            return View(obj);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            std s = null;
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+            var response = client.GetAsync("stulist?id="+id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<std>();
+                display.Wait();
+                s = display.Result;
+            }
+            return View(s);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(std s)
+        {
+          
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+            var response = client.PostAsJsonAsync<std>("stulist", s);
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            std s = null;
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+            var response = client.GetAsync("stulist?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<std>();
+                display.Wait();
+                s = display.Result;
+            }
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult Edit(std stud)
+        {
+           
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+            var response = client.PutAsJsonAsync<std>("stulist", stud);
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            return View("Edit");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            std s = null;
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+            var response = client.GetAsync("stulist?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<std>();
+                display.Wait();
+                s = display.Result;
+            }
+            return View(s);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public  ActionResult DeleteConfirm(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44392/api/stulist");
+            var response = client.DeleteAsync("stulist/" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+
+    }
+}
---
+++ MyNewAspWebApi/Controllers/StuListController.cs
@@ -0,0 +1,76 @@
+﻿using MyNewAspWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyNewAspWebApi.Controllers
+{
+    public class StuListController : ApiController
+    {
+        studentEntities db = new studentEntities();
+       [HttpGet]
+        public IHttpActionResult Action()
+        {
+            var obj = db.stds.ToList(); // var obj can be replaced by List<Stu> obj=
+            return Ok(obj);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetById(int id)
+        {
+            var obj = db.stds.Where(x => x.id == id).FirstOrDefault(); // var obj can be replaced by List<Stu> obj= and FirstOrDefalt() replaced by ToList();
+            return Ok(obj);
+        }
+        //[HttpGet] if you want get by id then we use
+        //  public IHttpActionResult Action(int id)
+        //  {
+        //      var obj = db.stds.Where(x => x.id == id).FirstOrDefault(); //in place of first or defalt we can use .ToList();
+        //      return Ok(obj);
+        //  }
+        [HttpPost]
+        public IHttpActionResult Insert(std stdins)
+        {
+            db.stds.Add(stdins);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(std stdins)
+        {
+            db.Entry(stdins).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            //OR 
+
+            //var data = db.stds.Where(x => x.id == stdins.id).FirstOrDefault();
+            //if (data != null)
+            //{
+            //    data.id = stdins.id;
+            //    data.name = stdins.name;
+            //    data.department = stdins.department;
+            //    data.gender = stdins.gender;
+            //    db.SaveChanges();
+            //}
+            //else
+            //{
+            //    return NotFound();
+            //}
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult StuDelete(int id)
+        {
+            var obj = db.stds.Where(x => x.id == id).FirstOrDefault();
+            db.Entry(obj).State = System.Data.Entity.EntityState.Deleted;
+            //var obj = db.stds.Find(id);
+            //db.stds.Remove(obj);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
